cb23e39edbc2c03d6d2b9f25baaf56ee3ec85694
Pedro-Augusto-Chaves <pedro.pedroza@cear.ufpb.br>
2024-07-20 12:22:34 -0300

    Adiciona dia exercicios do dia 6

---
+++ Dia 6/Exercise 3.py
@@ -0,0 +1,19 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    turn_right()
+    move()
+    turn_right()
+    move()
+
+while not at_goal():
+    if wall_on_right() and front_is_clear():
+        move()
+    elif right_is_clear():
+        jump()
+    else:
+        turn_left()
+        
